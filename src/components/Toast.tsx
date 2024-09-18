dce3742f21afb7ea53e86e9c88bcace2967c1846
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2024-09-18 23:31:57 +0200

    toast styling

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,7 +1,10 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
-import { CloseIcon, InfoIcon } from "@/svg/icons";
+import { CloseIcon } from "@/svg/icons";
 import classNames from "classnames";
+import { Bricolage_Grotesque } from "next/font/google";
 import { useEffect, useState } from "react";
+
+const bricolageGrotesque = Bricolage_Grotesque({ subsets: ["latin"] });
 
 function Toast() {
   const [show, setShow] = useState(false);
@@ -29,34 +32,44 @@ function Toast() {
   return (
     <div
       className={classNames(
-        "fixed z-30 bottom-24 lg:bottom-32 right-0 px-5 transition-transform duration-500",
+        "fixed z-30 min-w-[min(100%,425px)] bottom-0 left-1/2 -translate-x-1/2 lg:left-auto lg:right-0 px-3.5 pb-12 lg:p-10 transition-transform duration-500",
         {
-          "translate-x-full": !show,
+          "translate-y-full lg:translate-x-full": !show,
+          "lg:translate-x-0": show,
         }
       )}
     >
-      <div className="rounded-lg overflow-hidden border border-custom-beige bg-gray-900 max-w-96">
-        <div className="p-2 bg-custom-beige flex justify-between items-center text-black">
-          <div className="flex items-center ">
-            <InfoIcon className="w-5 h-5 fill-current" />
-            <div className=" text-sm ml-2 italic font-bold tracking-wide">
-              Hi there! Welcome
-            </div>
+      <div className="min-w-full h-full relative rounded-lg overflow-hidden custom-shadow border border-custom-beige bg-black max-w-96 text-white">
+        <div className="absolute inset-0 bg-custom-gray/10"></div>
+        <div className="relative pt-6 pb-6 pl-5 pr-9 flex">
+          <div className="">
+            <p
+              className={classNames(
+                "font-extrabold text-xl tracking-tight",
+                bricolageGrotesque.className
+              )}
+            >
+              So how does this work?
+            </p>
+            <p className="pt-4">
+              It's simple - get to know me better by playing a track. There's a
+              short visual text on each one.
+            </p>
+            <p className="pt-4">I suggest starting with the first one.</p>
+            <p className="pt-4">
+              Enjoy!
+              <br />
+              /Oscar
+            </p>
           </div>
-          <button
-            className="w-12 h-12 -m-6 group"
-            onClick={dismiss}
-            aria-label="Close this notification"
-          >
-            <CloseIcon className="w-6 h-6 stroke-current group-hover:opacity-70" />
-          </button>
         </div>
-        <div className="p-4">
-          Get to know me better by playing one of the tracks.
-          <br />
-          <br />
-          /Oscar
-        </div>
+        <button
+          className="absolute top-0 right-0 w-12 h-12 group flex justify-center items-center"
+          onClick={dismiss}
+          aria-label="Close this notification"
+        >
+          <CloseIcon className="w-6 h-6 stroke-current opacity-60 group-hover:opacity-100" />
+        </button>
       </div>
     </div>
   );
